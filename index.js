6055942908b64371896befde3ea8905126a55bc9
Shamim Ahmed <109693387+dev-shamim-ahamed@users.noreply.github.com>
2025-10-06 18:47:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 // Your frontend URL
-const FRONTEND_URL = 'https://178ql44r-5173.asse.devtunnels.ms';
+const FRONTEND_URL = 'https://primev1.vercel.app/';
 
 // Middleware
 app.use(cors({
@@ -471,5 +471,6 @@ process.on('unhandledRejection', (reason, promise) => {
 });
 
 module.exports = app;
+
 
 
